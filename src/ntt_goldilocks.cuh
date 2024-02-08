dfe0f87b630eb06aca628dc6e06aed6ee4a2a1ca
Dumi Loghin <dumitrel.loghin@okg.com>
2024-02-08 09:48:21 +0000

    add cuda header file

---
+++ src/ntt_goldilocks.cuh
@@ -0,0 +1,2 @@
+void init_twiddle_factors_temp(u_int32_t device_id, u_int32_t lg_n);
+void extendPol_temp(Goldilocks::Element *dst, Goldilocks::Element *src, uint64_t log_N_Extended, uint64_t log_N, uint64_t ncols);
